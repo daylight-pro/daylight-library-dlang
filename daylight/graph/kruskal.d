26a74f33f3c8601c2b3652a7b807c157f79a9728
Daylight <33833751+daylight-pro@users.noreply.github.com>
2024-11-22 17:52:21 +0900

    クラスカル法修正

--- daylight/graph/kruskal.d
+++ daylight/graph/kruskal.d
@@ -6,8 +6,8 @@ import acl.dsu;
 import daylight.base;
 import daylight.graph.base;
 
-long kruskal(int V, Edges!long ES) {
-    bool comp(Edge!long a, Edge!long b) {
+T kruskal(T)(int V, Edges!T ES) {
+    bool comp(Edge!T a, Edge!T b) {
         if (a.info != b.info)
             return a.info < b.info;
         if (a.from != b.from)
@@ -17,16 +17,13 @@ long kruskal(int V, Edges!long ES) {
 
     ES.sort!(comp);
 
-    long ret = 0;
+    T ret = 0;
     auto d = Dsu(V);
     foreach (e; ES) {
         if (!d.same(e.from, e.to)) {
             ret += e.info;
             d.merge(e.from, e.to);
         }
-    }
-    if (d.size(0) != V) {
-        return LINF;
     }
     return ret;
 }
---
+++ test/aoj/others/p2224.d
@@ -0,0 +1,33 @@
+module test.aoj.others.p2224;
+
+// verification-helper: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2224
+import std;
+import daylight.base;
+import daylight.graph.base;
+import daylight.graph.kruskal;
+
+void main() {
+    Reader reader = new Reader();
+    int N, M;
+    reader.read(N, M);
+    long[][] XY = new long[][](N, 2);
+    reader.read(XY);
+    long[][] PQ = new long[][](M, 2);
+    reader.read(PQ);
+    auto G = new Edges!double(N);
+    double dist(long u, long v) {
+        return ((XY[u][0] - XY[v][0]) ^^ 2 + (XY[u][1] - XY[v][1]) ^^ 2).to!double ^^ 0.5;
+    }
+
+    double sum = 0;
+    foreach (i; 0 .. M) {
+        long u = PQ[i][0] - 1;
+        long v = PQ[i][1] - 1;
+        sum += dist(u, v);
+        G ~= Edge!double(u.to!int, v.to!int, -dist(u, v));
+        G ~= Edge!double(v.to!int, u.to!int, -dist(v, u));
+    }
+
+    double d = kruskal!double(N, G);
+    writef("%.10f\n", sum + d);
+}
